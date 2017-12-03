e35f634b803f79435e7e30d46b637ab0b12d2e01
Piotr <kondrat.piotr@gmail.com>
2017-12-03 14:23:23 +0100

    asd

--- src/main/java/pl/piotrkondrat/repo/models/ContactBookModel.java
+++ src/main/java/pl/piotrkondrat/repo/models/ContactBookModel.java
@@ -20,7 +20,7 @@ public class ContactBookModel {
     private String lastname;
     private String gender;
     @Column(name = "contact_id")
-    @OneToMany(mappedBy = "contactId")
+//    @OneToMany(mappedBy = "contactId")
     private int contactId;
 
 }
--- src/main/java/pl/piotrkondrat/repo/models/repositories/ContactBookRepository.java
+++ src/main/java/pl/piotrkondrat/repo/models/repositories/ContactBookRepository.java
@@ -2,8 +2,9 @@ package pl.piotrkondrat.repo.models.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import pl.piotrkondrat.repo.models.ContactBookModel;
 
 @Repository
-public interface ContactBookRepository extends CrudRepository <ContactBookRepository, Integer>{
+public interface ContactBookRepository extends CrudRepository <ContactBookModel, Integer>{
 
 }
